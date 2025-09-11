a08edd426b9d9d629ba93981c59fe7bab226521a
Deekshitha_Y <deekshithakmit@gmail.com>
2025-09-11 13:21:56 +0530

    all maven files

---
+++ src/main/java/SE_Lab/Multiplayer_Gaming_Maven_App/App.java
@@ -0,0 +1,10 @@
+package SE_Lab.Multiplayer_Gaming_Maven_App;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Multiplayer Gaming Maven App");
+    }
+}
